4f95d7389b6983a280b29ff7e66d671b7e13327d
DJever23 <13262218151@163.com>
2020-03-19 12:20:40 +0800

    add notes

--- example.py
+++ example.py
@@ -8,17 +8,17 @@ from kmeans import kmeans, avg_iou
 # ANNOTATIONS_PATH = "./data/pascalvoc07-annotations"
 ANNOTATIONS_PATH = "./data/widerface-annotations"
 CLUSTERS = 25
-BBOX_NORMALIZE = True
+BBOX_NORMALIZE = False
 
 def show_cluster(data, cluster, max_points=2000):
 	'''
-	Display bouding box's size distribution and anchor generated in scatter.
+	Display bouding box's size distribution and anchor generated in scatter.散点图
 	'''
 	if len(data) > max_points:
 		idx = np.random.choice(len(data), max_points)
-		data = data[idx]
-	plt.scatter(data[:,0], data[:,1], s=5, c='lavender')
-	plt.scatter(cluster[:,0], cluster[:, 1], c='red', s=100, marker="^")
+		data = data[idx]#在所有的data中随机抽取max_points个数据
+	plt.scatter(data[:,0], data[:,1], s=5, c='lavender')#输入数据是data的宽和高，s=5是点的大小，c是颜色
+	plt.scatter(cluster[:,0], cluster[:, 1], c='red', s=100, marker="^")#‘^’是正三角形
 	plt.xlabel("Width")
 	plt.ylabel("Height")
 	plt.title("Bounding and anchor distribution")
@@ -27,17 +27,21 @@ def show_cluster(data, cluster, max_points=2000):
 
 def show_width_height(data, cluster, bins=50):
 	'''
-	Display bouding box distribution with histgram.
+	Display bouding box distribution with histgram.直方图
 	'''
 	if data.dtype != np.float32:
 		data = data.astype(np.float32)
 	width = data[:, 0]
+	print('width_in show_width_height)',len(width))
 	height = data[:, 1]
+	print('height in show_width_height',height)
 	ratio = height / width
 
-	plt.figure(1,figsize=(20, 6))
+	plt.figure(1,figsize=(20, 6))#num:图像编号或名称，数字为编号 ，字符串为名称；figsize:指定figure的宽和高，单位为英寸；
 	plt.subplot(131)
+	#subplot可以规划figure划分为n个子图，但每条subplot命令只会创建一个子图,131表示整个figure分成1行3列，共3个子图，这里子图在第一行第一列
 	plt.hist(width, bins=bins, color='green')
+	#width指定每个bin(箱子)分布的数据,对应x轴；bins这个参数指定bin(箱子)的个数,也就是总共有几条条状图；color指定条状图的颜色;默认y轴是个数
 	plt.xlabel('width')
 	plt.ylabel('number')
 	plt.title('Distribution of Width')
@@ -63,10 +67,13 @@ def sort_cluster(cluster):
 	'''
 	if cluster.dtype != np.float32:
 		cluster = cluster.astype(np.float32)
-	area = cluster[:, 0] * cluster[:, 1]
-	cluster = cluster[area.argsort()]
+	print('cluster',cluster)
+	area = cluster[:, 0] * cluster[:, 1]#计算每一个聚类中心点横纵坐标的乘积
+	cluster = cluster[area.argsort()]#argsort函数返回的是数组值从小到大的索引值，此处将cluster按从小到大进行排序
+	print('sorted cluster',cluster)
 	ratio = cluster[:,1:2] / cluster[:, 0:1]
-	return np.concatenate([cluster, ratio], axis=-1)
+	print('ratio',ratio)
+	return np.concatenate([cluster, ratio], axis=-1)  # 按轴axis连接array组成一个新的array,-1表示在最后一维进行合并，也就是行的方向合并
 
 
 def load_dataset(path, normalized=True):
@@ -74,8 +81,8 @@ def load_dataset(path, normalized=True):
 	load dataset from pasvoc formatl xml files
 	'''
 	dataset = []
-	for xml_file in glob.glob("{}/*xml".format(path)):
-		tree = ET.parse(xml_file)
+	for xml_file in glob.glob("{}/*xml".format(path)):#获取path路径下所有的xml文件并返回一个list
+		tree = ET.parse(xml_file)#调用parse()方法，返回解析树
 
 		height = int(tree.findtext("./size/height"))
 		width = int(tree.findtext("./size/width"))
@@ -99,12 +106,13 @@ def load_dataset(path, normalized=True):
 
 print("Start to load data annotations on: %s" % ANNOTATIONS_PATH)
 data = load_dataset(ANNOTATIONS_PATH, normalized=BBOX_NORMALIZE)
-
+print('{}'.format(type(data)))#<class 'numpy.ndarray'>,(144027, 2)
 print("Start to do kmeans, please wait for a moment.")
-out = kmeans(data, k=CLUSTERS)
+out = kmeans(data, k=CLUSTERS)#out为由kmeans找到的聚类中心点
 
 out_sorted = sort_cluster(out)
-print("Accuracy: {:.2f}%".format(avg_iou(data, out) * 100))
+print('out_sorted',out_sorted)
+print("Accuracy: {:.2f}%".format(avg_iou(data, out) * 100))#每个框与聚类中心点的最大IOU的平均值，可以用来表示所有框与聚类中心点的平均相似度
 
 show_cluster(data, out, max_points=2000)
 
--- kmeans.py
+++ kmeans.py
@@ -8,14 +8,15 @@ def iou(box, clusters):
     :param clusters: numpy array of shape (k, 2) where k is the number of clusters
     :return: numpy array of shape (k, 0) where k is the number of clusters
     """
-    x = np.minimum(clusters[:, 0], box[0])
-    y = np.minimum(clusters[:, 1], box[1])
+    x = np.minimum(clusters[:, 0], box[0])#取width最小值
+    y = np.minimum(clusters[:, 1], box[1])#取height最小值
     if np.count_nonzero(x == 0) > 0 or np.count_nonzero(y == 0) > 0:
+    #先判断x是否为0，是返回True，否则返回False，然后用np.count_nonzero()返回非零个数，如果非零个数>0，说明box里的宽或高有零值，则触发异常
         raise ValueError("Box has no area")
 
-    intersection = x * y
-    box_area = box[0] * box[1]
-    cluster_area = clusters[:, 0] * clusters[:, 1]
+    intersection = x * y#最小的宽高相乘得到交集面积
+    box_area = box[0] * box[1]#当前框面积
+    cluster_area = clusters[:, 0] * clusters[:, 1]#随机抽取的25个框的面积
 
     iou_ = intersection / (box_area + cluster_area - intersection)
 
@@ -28,6 +29,7 @@ def avg_iou(boxes, clusters):
     :param boxes: numpy array of shape (r, 2), where r is the number of rows
     :param clusters: numpy array of shape (k, 2) where k is the number of clusters
     :return: average IoU as a single float
+            返回：每个框与所有聚类中心点的iou取最大值，将这些最大值相加再取均值
     """
     return np.mean([np.max(iou(boxes[i], clusters)) for i in range(boxes.shape[0])])
 
@@ -55,13 +57,14 @@ def kmeans(boxes, k, dist=np.median):
     """
     rows = boxes.shape[0]
 
-    distances = np.empty((rows, k))
-    last_clusters = np.zeros((rows,))
+    distances = np.empty((rows, k))#返回(rows,k)形状的空数组
+    last_clusters = np.zeros((rows,))#返回(rows,)的全零数组
 
-    np.random.seed()
+    np.random.seed()#随机生成种子数
 
     # the Forgy method will fail if the whole array contains the same rows
-    clusters = boxes[np.random.choice(rows, k, replace=False)]
+    clusters = boxes[np.random.choice(rows, k, replace=False)]#在rows中随机抽取数字组成(k,)的一维数组，作为k个聚类中心，不能取重复数字
+    print("clusters id {}".format(clusters))
 
     iter_num = 1
     while True:
@@ -70,21 +73,33 @@ def kmeans(boxes, k, dist=np.median):
 
         for row in range(rows):
             distances[row] = 1 - iou(boxes[row], clusters)
+            #计算第row个box与随机抽取的25个box的iou，用此公式计算第row个box与随机抽取的25个box之间的距离
+        print('{}'.format(distances.shape))#(144027, 25)
 
-        nearest_clusters = np.argmin(distances, axis=1)
+        nearest_clusters = np.argmin(distances, axis=1)#按行取最小值索引,每一个框属于第几个聚类中心
+        print('nearest_clusters',nearest_clusters)
+        print('{}'.format(type(nearest_clusters)))#(144027,)
 
-        if (last_clusters == nearest_clusters).all():
+        if (last_clusters == nearest_clusters).all():#所有的返回值都为True才会执行，即当每个框属于某个聚类中心的索引不再更新时跳出循环
             break
 
         for cluster in range(k):
-            if len(boxes[nearest_clusters == cluster]) == 0:
+            print('len(boxes[nearest_clusters == cluster]):{}'.format(len(boxes[nearest_clusters == cluster])))#返回True的数量
+            #print('boxes[nearest_clusters == cluster]:{}'.format(boxes[nearest_clusters == cluster]))
+            #print('(nearest_clusters == cluster):{}'.format(nearest_clusters == cluster))
+            #[False False False ...  True  True False]
+            if len(boxes[nearest_clusters == cluster]) == 0:#
                 print("Cluster %d is zero size" % cluster)
                 # to avoid empty cluster
-                clusters[cluster] = boxes[np.random.choice(rows, 1, replace=False)]
+                clusters[cluster] = boxes[np.random.choice(rows, 1, replace=False)]#此聚类中心size为0时重新为当前位置随机选择一个聚类中心
                 continue
 
-            clusters[cluster] = dist(boxes[nearest_clusters == cluster], axis=0)
+            clusters[cluster] = dist(boxes[nearest_clusters == cluster], axis=0)#dist=np.median,在列的方向上求中位数
+            #clusters[cluster] = np.median(boxes[nearest_clusters == cluster], axis=0)
+            print('clusters[cluster]:{}'.format(clusters[cluster]))#[0.015625   0.02635432]
+            #print('clusters[cluster]:{}'.format(clusters[cluster]))
 
         last_clusters = nearest_clusters
+        #返回的是每一个聚类中心重新计算中位数，反复迭代计算后的新聚类中心点
 
     return clusters
